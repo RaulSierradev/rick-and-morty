4139429df397d70f4bcd61f685f78b516327b79b
RaulSierradev <illosierra@outlook.com>
2023-04-24 23:00:18 -0500

    ajustes navbar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,10 +1,13 @@
-import style from './SearchBar.module.css'
+import style from "./SearchBar.module.css";
 
 export default function SearchBar(props) {
-   return (
-      <div className={style.searchBar}>
-         <input type='search' />
-         <button onClick={props.onSearch}>Agregar</button>
+  return (
+    <div className={style.searchBar}>
+      <h1>Rick and Morty</h1>
+      <div className={style.searchSpace}>
+        <input type="search" />
+        <button onClick={props.onSearch}>Agregar</button>
       </div>
-   );
+    </div>
+  );
 }
