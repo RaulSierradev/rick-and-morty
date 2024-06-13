9a979c1869d9f43914726c673726c32754cdf5c8
RaulSierraDev <illosierra@outlook.com>
2024-06-12 22:57:07 -0500

    migracion a Vitejs

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
@@ -6,11 +5,10 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
